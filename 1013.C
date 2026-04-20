1ac6ad92bb7c48ad7f4de9176e1ca4820e224852
Danilo-Silva-Cardoso <danilo.cardoso@unifesp.br>
2026-04-20 20:40:33 -0300

    Update 1013.C

--- 1013.C
+++ 1013.C
@@ -8,18 +8,19 @@ Objetivo    : Descobrir qual é o maior número entre 3 valores
 Aprendizado : utilizar a fórmula para descobrir o maior
 -------------------------------------------------------------------------- */
 
-#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    int a , b , c , maior;
-    scanf ("%d", &a);
-    scanf ("%d" , &b);
-    scanf ("%d" , &c);
+int main() {
+    int a, b, c, maior;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
     
-    maior = (a+b+abs(a-b))/2 ;
-    maior = (maior+c+abs(maior-c))/2 ;
+    maior = (a + b + abs(a - b)) / 2;
+    maior = (maior + c + abs(maior - c)) / 2;
     
     printf("%d eh o maior\n", maior);
+
     return 0;
 }
