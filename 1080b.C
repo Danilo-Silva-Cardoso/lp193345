f1c88e5d56ff736f47c07a0b64bcbb144ae825dd
Danilo-Silva-Cardoso <danilo.cardoso@unifesp.br>
2026-04-20 20:39:22 -0300

    Update 1080b.C

--- 1080b.C
+++ 1080b.C
@@ -5,25 +5,22 @@ Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1080
 Data        : 20/04/2026
 Objetivo    : Encontrar o maior valor entre 100 números armazenados em um vetor e indicar sua posição
-Aprendizado : Aprendizado : Uso de vetores, laço de repetição (for), estrutura condicional (if) e manipulação de índices
+Aprendizado : Uso de vetores, laço de repetição (for), estrutura condicional (if) e manipulação de índices
 -------------------------------------------------------------------------- */
 
 #include <stdio.h>
 
 int main() {
-    int vetor[100];
-    int maior, posicao = 1;
+    int valor, maior, posicao = 1;
 
-    for (int i = 0; i < 100; i++) {
-        scanf("%d", &vetor[i]);
-    }
+    scanf("%d", &maior);
 
-    maior = vetor[0];
+    for (int i = 2; i <= 100; i++) {
+        scanf("%d", &valor);
 
-    for (int i = 1; i < 100; i++) {
-        if (vetor[i] > maior) {
-            maior = vetor[i];
-            posicao = i + 1;
+        if (valor > maior) {
+            maior = valor;
+            posicao = i;
         }
     }
 
