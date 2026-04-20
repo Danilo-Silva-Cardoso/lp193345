74dff4876b40feeadb14d28efb5f2e46d64ae803
Danilo-Silva-Cardoso <danilo.cardoso@unifesp.br>
2026-04-20 20:32:48 -0300

    Update 1080a.C

--- 1080a.C
+++ 1080a.C
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Danilo Da Silva Cardoso
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1071
+Data        : 20/04/2026
+Objetivo    : Identificar o maior valor entre 100 números inteiros e sua posição de entrada
+Aprendizado : Uso de laço de repetição (for), estrutura condicional (if) e variáveis para armazenar maior valor e sua posição
+-------------------------------------------------------------------------- */
+
 //sem vetor
 
 #include <stdio.h>
