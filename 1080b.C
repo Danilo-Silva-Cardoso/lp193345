2f5f61c4800d13815f8ab94f193f646091bf76cb
Danilo-Silva-Cardoso <danilo.cardoso@unifesp.br>
2026-04-20 20:34:29 -0300

    Update 1080b.C

--- 1080b.C
+++ 1080b.C
@@ -1,4 +1,12 @@
-//com vetor
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Danilo Da Silva Cardoso
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Data        : 20/04/2026
+Objetivo    : Encontrar o maior valor entre 100 números armazenados em um vetor e indicar sua posição
+Aprendizado : Aprendizado : Uso de vetores, laço de repetição (for), estrutura condicional (if) e manipulação de índices
+-------------------------------------------------------------------------- */
 
 #include <stdio.h>
 
