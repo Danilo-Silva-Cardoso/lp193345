b37919b133556e60d7955245fda54df645d595e9
Danilo-Silva-Cardoso <danilo.cardoso@unifesp.br>
2026-04-28 22:14:00 -0300

    Update and rename 1078>C to 1078.C

---
+++ 1078.C
@@ -0,0 +1,28 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Danilo Da Silva Cardoso
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1078
+Data        : 28/04/2026
+Objetivo    : Gerar e exibir a tabuada de um número inteiro N, mostrando os resultados das multiplicações de 1 até 10.
+Aprendizado : Uso de laço de repetição (for) para percorrer os valores de 1 a 10, realização de operações matemáticas (multiplicação) e formatação da saída para exibir os resultados no padrão solicitado.
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+ 
+int main() {
+    int valorfixo,valor,i;
+    
+    scanf("%d", &valorfixo) ;
+        if (2<valorfixo && valorfixo <1000) {
+            
+            for(i=1;i<=10;i++) {
+             
+                valor=valorfixo*i;
+                printf("%d x %d = %d\n", i, valorfixo, valor);
+            
+            }
+        }
+    return 0;
+}
+
