2fdf561d7300b3b179de52de14ae75e5f370fafd
Danilo-Silva-Cardoso <danilo.cardoso@unifesp.br>
2026-05-05 21:32:09 -0300

    Update 1241.C

--- 1241.C
+++ 1241.C
@@ -1,3 +1,14 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Danilo Da Silva Cardoso
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1241
+Data        : 05/05/2026
+Objetivo: Verificar se B é o final de A.
+Aprendizado: Trabalhar com números grandes como strings para comparação sem conversão numérica.
+-------------------------------------------------------------------------- */
+
+
 #include <stdio.h>
 #include <string.h>
 
