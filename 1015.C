86fd367e5e0fa11d826a1a93e6fe8f2a56e10454
Danilo-Silva-Cardoso <danilo.cardoso@unifesp.br>
2026-05-14 14:56:26 -0300

    Update 1015.C

--- 1015.C
+++ 1015.C
@@ -5,11 +5,18 @@ Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1015
 Data        : 14/05/2026
 Objetivo    : Distância Entre Dois Pontos
-Aprendizado : Utilizar o math.h , manipulação de double e ultilizar 4 casas decimais
+Aprendizado : Utilizar o math.h , manipulação de double e ultilizar 4 casas decimais e função
 -------------------------------------------------------------------------- */
 
 #include <stdio.h>
 #include <math.h>
+
+double calculardistancia(double x1, double x2, double y1, double y2) {
+    double dx = x2 - x1;
+    double dy = y2 - y1;
+    
+    return sqrt((dx * dx) + (dy * dy));
+}
  
 int main() {
  
@@ -20,14 +27,9 @@ int main() {
     scanf("%lf %lf", &x1, &y1);
     scanf("%lf %lf", &x2, &y2);
     
-    p1 = (x2-x1)*(x2-x1);
-    
-    p2 = (y2-y1)*(y2-y1);
-    
-    distancia = sqrt(p1+p2);
+    distancia = calculardistancia(x1, x2, y1, y2);
     
     printf("%.4lf\n", distancia);
     
     return 0;
 }
-
