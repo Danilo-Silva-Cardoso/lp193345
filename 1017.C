0c82ad8cf2956ae7c4b795f98e1d4f73a29ab4ff
Danilo-Silva-Cardoso <danilo.cardoso@unifesp.br>
2026-05-20 22:36:25 -0300

    Update 1017.C

--- 1017.C
+++ 1017.C
@@ -2,8 +2,8 @@
 Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Danilo Da Silva Cardoso
 Linguagem   : C
-Problema    : https://judge.beecrowd.com/pt/problems/view/1000
-Data        : 30/03/2026
+Problema    : https://judge.beecrowd.com/pt/problems/view/1017
+Data        : 20/05/2026
 Objetivo    : Calcular a quantidade de litros de combustível gastos em uma viagem com base no tempo e na velocidade média.
 Aprendizado : Aprendi a utilizar funções em C, realizar cálculos matemáticos, trabalhar com variáveis do tipo inteiro e float,
 além de usar scanf e printf para entrada e saída de dados.
