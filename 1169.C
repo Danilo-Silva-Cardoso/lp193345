e571d8e13c6088404b03b8496006479a154c791d
Danilo-Silva-Cardoso <danilo.cardoso@unifesp.br>
2026-06-09 16:43:01 -0300

    Create 1169.C

---
+++ 1169.C
@@ -0,0 +1,49 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Danilo Da Silva Cardoso
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1169
+Data        : 09/06/2026
+Objetivo    : Mostrar o Hello Word para o usuário
+Aprendizado : Aprendi a funcionalidade do printf
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+
+unsigned long long quantgrao(unsigned long long casas, unsigned long long graos , unsigned long long total) {
+    
+    if ( casas == 0) {
+        
+        return total / 12000 ;
+        
+    } else {
+        
+        return quantgrao(casas - 1, graos * 2, total + graos);
+        
+    }
+}
+
+int main() {
+    int testes;
+    int casas;
+
+    scanf("%d", &testes);
+
+    while (testes > 0) {
+
+        scanf("%d", &casas);
+
+        unsigned long long graos = 1;
+        unsigned long long total = 0;
+        
+        total = quantgrao(casas, graos, total);
+        
+        printf("%llu kg\n", total);
+
+        testes--;
+    }
+    
+    
+    return 0;
+    
+}
