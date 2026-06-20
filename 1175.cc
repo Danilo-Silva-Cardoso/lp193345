d1c05ecc49250720a32f00e47c8b3a7ebc45361e
Danilo-Silva-Cardoso <danilo.cardoso@unifesp.br>
2026-06-20 10:59:55 -0300

    Update 1175.cc

--- 1175.cc
+++ 1175.cc
@@ -1,3 +1,14 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Danilo Da Silva Cardoso
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1175
+Data        : 20/06/2026
+Objetivo    : inverter a ordem do vetor;
+Aprendizado : Usar uma variavel aux para ajudar na logica; 
+Duvidas :  Um pouco na lógica;
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
  
 int main() {
