4fe4ad0f425f57b1734f7be398e985939cb71773
Danilo-Silva-Cardoso <danilo.cardoso@unifesp.br>
2026-06-20 21:19:36 -0300

    Update 1441.C

--- 1441.C
+++ 1441.C
@@ -3,37 +3,45 @@ Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : Danilo Da Silva Cardoso
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1441
-Data        : 11/06/2026
+Data        : 20/06/2026
 Objetivo    : Sequêncua de Granizo
 Aprendizado : utilizar recursividade
 -------------------------------------------------------------------------- */
 
-#include <stdio.h>
-
-long long granizo(long long h) {
-    if (h == 1)
-        return 1;
-
-    long long proximo;
-
-    if (h % 2 == 0)
-        proximo = h / 2;
-    else
-        proximo = 3 * h + 1;
-
-    long long maior = granizo(proximo);
-
-    if (h > maior)
-        return h;
-
-    return maior;
+int sequencia(int h, int valor) {
+    
+    if(h == 1) {
+        return valor;
+    }
+    
+    if(h % 2 == 0) {
+        h = h*0.5;
+        if(h > valor) {
+            valor = h;
+        }
+    }
+    else {
+        h = 3*h + 1;
+        if(h > valor) {
+            valor = h;
+        }
+    }
+    
+    return sequencia(h, valor);
 }
 
+#include <stdio.h>
+ 
 int main() {
-    long long H;
-
-    while (scanf("%lld", &H) && H != 0) {
-        printf("%lld\n", granizo(H));
+    
+    int valor;
+ 
+    int h;
+    
+    while(scanf("%d", &h) == 1 && h != 0) {
+        valor = h;
+        int resultado = sequencia(h, valor);  
+        printf("%d\n", resultado);
     }
 
     return 0;
